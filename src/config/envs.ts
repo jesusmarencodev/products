0518b22455968571ccf18ee33284295fe6a075f3
Jesus Marenco <jesusmarencodev@gmail.com>
2024-04-05 17:50:51 -0500

    config Environment Variables

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,6 +1,27 @@
 import 'dotenv/config';
+import * as joi from 'joi';
 
+interface EnvVars {
+  PORT: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true); // This unknown(true) will accept more environment variables although they are not defined here for my taste and should not be
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Config validtion error ${error.message}`);
+}
+
+const envVars: EnvVars = value;
+
+//console.log(error);
+//console.log(envValues);
 
 export const envs = {
-    port: process.env.PORT || 3000,
-}
+  port: envVars.PORT || 3000,
+};
